ce2878f797308feef0cc482ad84964e02be29902
gaoyuan <1059079892@qq.com>
2016-07-21 22:58:00 +0800

    播放音频

--- src/main/webapp/src/js/practice.js
+++ src/main/webapp/src/js/practice.js
@@ -1,17 +1,29 @@
+
 $(document).ready(function () {
     $.init();
+    function bofang()
+        {
+        var x = document.getElementById("audion");
+        x.play();
+        }
+        function zhanting()
+        {
+        var x = document.getElementById("audion");
+        x.pause();
+        }
     console.log("哈哈哈");
     $(".playn").on('click',function(){
         $(".playn").hide();
         $(".stopn").show();
-        var x = document.getElementsByClassName("audion");
-        window,close();
-        x.play();
+        bofang();
+        // $(".stopn").style.display="block";
+        // var x = document.getElementsByClassName("audion");
+        // x.play();
     })
     $(".stopn").click(function(){
         $(".stopn").hide();
         $(".playn").show();
-        $(".audion").pause();
+        zhanting();
     })
 
 })
