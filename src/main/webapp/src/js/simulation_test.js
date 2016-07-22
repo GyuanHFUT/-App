ff3438f632c37b4fd95e210707c3e058092fdcf5
summer-Yang163 <2448415727@qq.com>
2016-07-22 16:47:44 +0800

    补充页面grade及弹出层页面

--- src/main/webapp/src/js/simulation_test.js
+++ src/main/webapp/src/js/simulation_test.js
@@ -1,33 +1,14 @@
-// window.onload=function(){
-//    alert("hah");
-//   $.init();
-//    var data ={
-//      "title":"专项学习"
-//    };
-//
-//    var myTemplate = Handlebars.compile($("#myTemplate").html());
-//    $("#12").html(myTemplate(data));
-//
-// }
 $(document).ready(function(){
   var data ={
     "title":"专项学习"
   };
-  console.log(data);
   var myTemplate = Handlebars.compile($("#myTemplate").html());
   $("#content").html(myTemplate(data));
-$.init();
-            // function show(t) {
-            //      $("#content").html(t);
-            //     }
-            //
-            //  var view = {
-            //        title: 'YZF',
-            //        cacl: function () {
-            //           return 6 + 4;
-            //        }
-            //  };
-            //  $("#content").html(Mustache.render("{{title}} spends {{cacl}}", view));
-
+  $.init();
+  $(document).on('click','.confirm-ok', function () {
+    $.confirm('确定交卷?', function () {
+      $.router.load("./grade.html");
+    });
+  });
 
 })
