13523c469399773afffda584728e547c17fa0147
gaoyuan <1059079892@qq.com>
2016-07-24 14:36:36 +0800

    xiugai

--- src/main/webapp/src/js/exercise1.js
+++ src/main/webapp/src/js/exercise1.js
@@ -1,40 +1,40 @@
 
 $(document).ready(function () {
-    
-    $(".stopn").click(function(){
-        $(".stopn").hide();
-        $(".playn").show();
-        zhanting();
-    })
-    
-    // function adddui (arr){
-    //     for (var i = arr.length - 1; i >= 0; i--) {
-    //         if(arr[i].hasClass("dui")) {
-    //             $(".select")[i].addClass("duicolor");
-    //         }
-    //     };
-    // }
+
     $(".select").one('click',function(){
+        var x=$(this).attr('value');
+        var tihao=parseInt(x);
+        var flag=$(tihao).attr('value');
+        var parents  =  $(this).parent().parent();
+        var parentss=parents.parent();   
+        console.log( typeof x);     
+        // console.log($('.flex[value="x"]'));   
+        // console.log( $('.flex[value="1"]'));  
+       console.log( $('.flex:eq(1)'))
+       console.log( $('.flex:eq(tihao)'))
         if ($(this).hasClass("dui")) {
             $(this).find('i').html('&#xe61b;');
             //在popup层找到相对应的box改变颜色！
             $(this).addClass("duicolor");
-            //自动下一页
+
+            // $('.flex[value='1']').addClass("popdui");
+            $('.flex[value="tihao"]').addClass("popdui");
+            //自动下一页，然后改变box当前页面，并且播放语音
         }
         else{
              $(this).addClass("cuocolor");
              $(this).find('i').html('&#xf0011');
              var parent  =  $(this).parent();
-
+            $('.flex[value=tihao]').addClass("popcuo");
               for (var i =parent.find(".select ").length - 1; i >= 0; i--) {
                 var a=parent.find(".select ")[i];
                  if ($(a).hasClass("dui")) 
                     {
                        $(a).addClass("duicolor");
                     };
-                 $(".xiangjie-wapper").show();
+                     parents.find(".xiangjie-wapper").show();
+                     parentss.find(".open-xiangjie").addClass("active");
                 }
         }})
-
 })
 
