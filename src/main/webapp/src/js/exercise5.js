0fa88a0b3d8e1981f174fc7731c6f9692aa574b5
gaoyuan <1059079892@qq.com>
2016-08-03 08:12:03 +0800

    完善细节

--- src/main/webapp/src/js/exercise5.js
+++ src/main/webapp/src/js/exercise5.js
@@ -2,71 +2,82 @@
 $(document).ready(function () {
 
     var data = [
-                          { 
-                              "listen_id":1,
-                              "listen_type":1,
+                          {
+                             "listen_id":1,
+                             "listen_type":"3",
+                             "listen_title":"",
+                             "option_A": "sunshine",
+                             "option_B": "big",
+                             "option_C": "two",
+                             "listen_answer":"B",
+                             "radio_url":"../src/audio/1.mp3",
+                             "answer":"违反道路交通安全法，违反法律法规即为内联的新页面违法行为。官方已无违章/违规的说法。",
+                             "yuanwen":"Is there anything wrong with you,Peter?",
+                             "listen_style":"1",
+                             "form_url":"../src/img/encouragement.jpg"
+                         },
+                          {
+                              "listen_id":2,
+                              "listen_type":2,
+                             "listen_title":"What will the weather be like nowday?",
                               "option_A": "sunshine",
                               "option_B": "big",
                               "option_C": "boy",
-                              "listen_answer":"A",
-                              "radio_url":"../src/audio/1.mp3",
-                              "answer":"违反道路交通安全法，违反法律法规即为内联的新页面违法行为。官方已无违章/违规的说法。",
-                              "yuanwen":"Is there anything wrong with you,Peter?"
-                          },
-                          {
-                              "listen_id":2,
-                              "listen_type":1,
-                              "option_A": "sunshine",
-                              "option_B": "big",
-                              "option_C": "two",
                               "listen_answer":"B",
                               "radio_url":"../src/audio/1.mp3",
                               "answer":"违反道路交通安全法，违反法律法规即为内联的新页面违法行为。官方已无违章/违规的说法。",
-                              "yuanwen":"Is there anything wrong with you,Peter?"
+                              "yuanwen":"Is there anything wrong with you,Peter?",
+                               "form_url":"../src/img/encouragement.jpg",
+                             "listen_style":"1"
                           },
                           {
                               "listen_id":3,
-                              "listen_type":1,
-                              "option_A": "sunshine",
-                              "option_B": "big",
-                              "option_C": "three",
+                              "listen_type":2,
+                             "listen_title":"What will the weather be like tomorrow?",
+                             "option_A": "../src/img/encouragement.jpg",
+                             "option_B": "../src/img/encouragement.jpg",
+                             "option_C": "../src/img/encouragement.jpg",
                               "listen_answer":"C",
                               "radio_url":"../src/audio/1.mp3",
                               "answer":"违反道路交通安全法，违反法律法规即为内联的新页面违法行为。官方已无违章/违规的说法。",
-                              "yuanwen":"Is there anything wrong with you,Peter?"
-                          }
+                              "yuanwen":"Is there anything wrong with you,Peter?",
+                               "form_url":"../src/img/encouragement.jpg",
+                                "listen_style":"2"
+                          },
+                          { "listen_id":4,
+                             "listen_type":2,
+                             "listen_title":"What will the weather be like future?",
+                             "option_A": "../src/img/encouragement.jpg",
+                             "option_B": "../src/img/encouragement.jpg",
+                             "option_C": "../src/img/encouragement.jpg",
+                             "listen_answer":"A",
+                             "radio_url":"../src/audio/1.mp3",
+                             "answer":"违反道路交通安全法，违反法律法规即为内联的新页面违法行为。官方已无违章/违规的说法。",
+                             "yuanwen":"Is there anything wrong with you,Peter?",
+                              "form_url":"../src/img/encouragement.jpg",
+                             "listen_style":"2"
+                         }
                        ] ;
       data[0].xx="page-current";
       data[0].box = "current";
       
       for (var t = 0; t <=data.length - 1 ; t++) {
-        var flag=data[t].listen_answer; 
-        switch (flag)
-        {
-        case "A":
-          data[t].A="dui";
-          break;
-        case "B":
-          data[t].B="dui";
-          break;
-        case "C":
-          data[t].C="dui";
-          break;
-        };
-        // var x="<li class="flex">"+(i+1)+"</li>";
-        // $("#hbs2").append(x);
-        //字符串拼接方法，待验证
+        s = data[t].listen_style;
+       switch(s){
+          case "1":data[t]["selects_type"]="words";break;
+          case "2":data[t]["selects_type"]="imgs"; break;
+          case "3":data[t]["selects_type"]=""; break;
+      };
+
       }
-        
-        //注册一个Handlebars模版，通过id找到某一个模版，获取模版的html框架
-        //$("#table-template").html()是jquery的语法，不懂的童鞋请恶补。。。
+
         var myTemplate = Handlebars.compile($("#myTemplate").html());
         var myTemplate2 = Handlebars.compile($("#myTemplate2").html());
         //注册一个Handlebars Helper,用来将索引+1，因为默认是从0开始的
         Handlebars.registerHelper("addOne",function(index,options){
           return parseInt(index)+1;
         });
-        
+
         //将json对象用刚刚注册的Handlebars模版封装，得到最终的html，插入到基础table中。
         $('#handlebars').html(myTemplate(data));
         $('#hbs2').html(myTemplate2(data));
@@ -74,7 +85,11 @@ $(document).ready(function () {
 
        var dui=0;
        var cuo=0;
-       var zong=$(".weida").find('b').html();
+
+       var zong=data.length;
+        var s=$(".yeshu").html();
+        console.log(s);
+        $(".weida").find('b').html(zong);
         $(".select").on('tap',function(){
         var parent  =  $(this).parent();
         var parents  =  $(this).parent().parent();
