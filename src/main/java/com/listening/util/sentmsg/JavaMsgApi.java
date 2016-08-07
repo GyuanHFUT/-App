46a552a4f306d8f079705d319e74c195df104c30
wangxiang <2206237787@qq.com>
2016-08-07 19:06:30 +0800

    xiugai

--- src/main/java/com/listening/util/sentmsg/JavaMsgApi.java
+++ src/main/java/com/listening/util/sentmsg/JavaMsgApi.java
@@ -1,7 +1,19 @@
 package com.listening.util.sentmsg;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,7 +32,7 @@ public class JavaMsgApi {
     }
 
     private static String post(String url, Map<String, String> paramsMap) throws IOException {
-        /*CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpClient client = HttpClients.createDefault();
         String responseMsg = "";
         CloseableHttpResponse response = null;
         try{
@@ -52,7 +64,7 @@ public class JavaMsgApi {
                e.printStackTrace();
             }
         }
-        return responseMsg;*/
-        return null;
+        return responseMsg;
+        //return null;
     }
 }
