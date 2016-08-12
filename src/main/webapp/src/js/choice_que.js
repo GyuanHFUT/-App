37480cc747bbb628ba3824a213355c4883146fb4
summer-Yang163 <2448415727@qq.com>
2016-08-12 16:30:38 +0800

    模拟考试登陆

--- src/main/webapp/src/js/choice_que.js
+++ src/main/webapp/src/js/choice_que.js
@@ -11,4 +11,25 @@ $(document).ready(function () {
               $.hidePreloader();
           }, 2000);
         });
+    $('#simulation').tap(function(){
+        $.ajax({
+                type: 'get',
+                url: "/JuniorHearing/collect/addCollect/",
+                data:'',
+                success: function(data){
+                    if(data=="login"){
+                        $.confirm('模拟考试需要登录，是否登陆?',
+                            function () {
+                                $.router.load("../pages/land.html");
+                            }
+                        );
+                    }else{
+                        if(data.success){
+                            $.router.load("../pages/simulation_test.html#1");
+                        }
+                    }
+                }
+            })
+
+    })
 })
